b6ecfe52a6be7c0ad3bef4d0924310caaa358a38
thomasondr <thomasondr@192.168.200.108>
2015-09-27 12:52:59 +0200

    Finished the third and fourth tests

--- src/main/java/hu/unimiskolc/iit/distsys/VMCreationClass.java
+++ src/main/java/hu/unimiskolc/iit/distsys/VMCreationClass.java
@@ -1,10 +1,13 @@
 package hu.unimiskolc.iit.distsys;
 
 import hu.mta.sztaki.lpds.cloud.simulator.Timed;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.IaaSService;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.PhysicalMachine.ResourceAllocation;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.VirtualMachine;
 import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.AlterableResourceConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ConstantConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.ResourceConstraints;
 import hu.mta.sztaki.lpds.cloud.simulator.io.VirtualAppliance;
 
 public class VMCreationClass implements VMCreationApproaches {
@@ -54,14 +57,50 @@ public class VMCreationClass implements VMCreationApproaches {
 
 	@Override
 	public void indirectVMCreation() throws Exception {
-		// TODO Auto-generated method stub
 		
+		PhysicalMachine pm = ExercisesBase.getNewPhysicalMachine();
+		pm.turnon();
+		
+		IaaSService iaas = ExercisesBase.getNewIaaSService();
+		iaas.registerHost(pm);
+		iaas.registerRepository(pm.localDisk);
+		
+		Timed.simulateUntilLastEvent();
+		
+		VirtualAppliance va = new VirtualAppliance("NewVA",1,0);
+		
+		pm.localDisk.registerObject(va);
+		
+		iaas.requestVM(va, new AlterableResourceConstraints(1, 2, 2), pm.localDisk, 2);
+		
+		Timed.simulateUntilLastEvent();
+
+				
 	}
 
 	@Override
 	public void migratedVMCreation() throws Exception {
-		// TODO Auto-generated method stub
 		
+		PhysicalMachine pm1 = ExercisesBase.getNewPhysicalMachine();
+		PhysicalMachine pm2 = ExercisesBase.getNewPhysicalMachine();
+		pm1.turnon();
+		pm2.turnon();
+		
+		Timed.simulateUntilLastEvent();
+		
+		VirtualAppliance va = new VirtualAppliance("NewVA",1,0);
+		pm1.localDisk.registerObject(va);
+		pm2.localDisk.registerObject(va);
+		
+		ResourceConstraints rc = new ConstantConstraints(1, 2, 2);
+		VirtualMachine vm = pm1.requestVM(va, rc, pm1.localDisk,1)[0];
+		
+		Timed.simulateUntilLastEvent();
+				
+		pm1.migrateVM(vm, pm2);
+		
+		Timed.simulateUntilLastEvent();
+				
 	}
 
 }
